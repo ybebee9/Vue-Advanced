899046780ac73c36516f06782262dec6b96bfaf1
ybebee9 <ybebee9@gmail.com>
2020-03-19 01:15:31 +0900

    스토어 구현

---
+++ vue-news/src/store/actions.js
@@ -0,0 +1,25 @@
+import {fetchAskList, fetchJobsList, fetchNewsList} from "../api";
+
+export default {
+    FETCH_NEWS({commit}) {
+        fetchNewsList()
+            .then(({data}) => {
+                commit('SET_NEWS', data);
+            })
+            .catch(error => console.log(error));
+    },
+    FETCH_JOBS({commit}) {
+        fetchJobsList()
+            .then(({data}) => {
+                commit('SET_JOBS', data);
+            })
+            .catch(error => console.log(error));
+    },
+    FETCH_ASK({commit}){
+        fetchAskList()
+            .then(({data}) => {
+                commit('SET_ASK', data);
+            })
+            .catch(error => console.log(error));
+    }
+}
